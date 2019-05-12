92f2353e1e5e276af443a8fa1c8d83b131a25766
kaye <pengjin250@gmail.com>
2019-05-12 10:31:17 +0800

    104

---
+++ 104.maximum_depth_of_binary_tree.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int weighttree(struct TreeNode* root);
+int max(int a, int b);
+
+bool isBalanced(struct TreeNode* root){
+    if(!root)return true;
+    if(abs(weighttree(root->left) - weighttree(root->right)) >1) return false;
+    return isBalanced(root->left) && isBalanced(root->right);
+
+}
+
+int weighttree(struct TreeNode* root){
+    if(!root)return 0;
+    return 1+ max(weighttree(root->left),weighttree(root->right));
+}
+
+int max(int a, int b){
+    if(a>=b)return a;
+    else return b;
+}
+
